9d214d6e5351992eb2a5da5f6c471ddabbcadb0b
bansidasabi007-netizen <bansidasabi007@gmail.com>
2026-01-08 12:02:41 +0530

    project 1 convertor

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int sec,h,m,s;
+
+    cout<<"Enter seconds:";
+    cin>>sec;
+    
+    h = sec/3600;
+    m = (sec % 3600)/60;
+    s = sec%60;
+    cout<<h<<":"<<m<<":"<<s;
+}
